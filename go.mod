33c1e0992c9f75a2b34720fee671bfdfc0cf7df1
Shinichi Yanagido <s.yanagido@gmail.com>
2022-07-09 01:20:04 +0900

    add test code

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/gidoichi/yaml-path
 go 1.17
 
 require (
+	github.com/onsi/ginkgo/v2 v2.1.4
+	github.com/onsi/gomega v1.19.0
 	github.com/urfave/cli/v2 v2.10.3
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -12,5 +14,9 @@ require (
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ main_test.go
@@ -0,0 +1,46 @@
+package main_test
+
+import (
+	main "github.com/gidoichi/yaml-path"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Main", func() {
+	yaml := []byte(`top:
+  first:
+    - name: myname
+      attr1: val1
+      attr2: val2
+      #       ^
+    - value2
+    - value3
+  second:
+    child1: value1
+    child1: value2
+    child3: value3
+`)
+
+	Context("Indicating at some token with Bosh format", func() {
+		It("should return the string path to the token in Bosh format.", func() {
+			path, err := main.PathAtPoint(5, 14, yaml, main.Bosh)
+			Expect(err).To(BeNil())
+			Expect(path).To(Equal("/top/first/name=myname/attr2"))
+		})
+	})
+
+	Context("Indicating at some token with JsonPath format", func() {
+		It("should return the string path to the token in JsonPath format.", func() {
+			path, err := main.PathAtPoint(5, 14, yaml, main.JsonPath)
+			Expect(err).To(BeNil())
+			Expect(path).To(Equal("$.top.first[0].attr2"))
+		})
+	})
+
+	Context("Indicating at no token", func() {
+		It("should return token not found error.", func() {
+			_, err := main.PathAtPoint(6, 14, yaml, main.Bosh)
+			Expect(err).NotTo(BeNil())
+		})
+	})
+})

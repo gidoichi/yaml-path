2064c0145f2f9e191a0100fdccb6c766f26664a5
Shinichi Yanagido <32694823+gidoichi@users.noreply.github.com>
2024-01-27 12:40:04 +0900

    enable automerge by dependabot (#49)
    Co-authored-by: Shinichi Yanagido <hakuto1yds@gmail.com>

---
+++ github/main.tf
@@ -0,0 +1,42 @@
+variable "GITHUB_TOKEN" {
+  type = string
+}
+
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = ">= 5.31.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.GITHUB_TOKEN
+  owner = "gidoichi"
+}
+
+resource "github_repository" "this" {
+  name                        = "yaml-path"
+  allow_auto_merge            = true
+  allow_merge_commit          = false
+  allow_rebase_merge          = false
+  delete_branch_on_merge      = true
+  description                 = "Get \"xpath\" for a given line at column of a YAML file"
+  has_issues                  = true
+  squash_merge_commit_message = "BLANK"
+  squash_merge_commit_title   = "PR_TITLE"
+}
+
+resource "github_branch_protection" "default" {
+  repository_id = github_repository.this.node_id
+  pattern       = "main"
+  required_status_checks {
+    strict = true
+    contexts = [
+      "go-test",
+      "no-diff",
+      "terraform-plan",
+    ]
+  }
+}
